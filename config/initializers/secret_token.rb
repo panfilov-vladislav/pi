91844647f39337e5bdb17d809af9decf3eeb74d7
panfilov <alias.co>
2011-05-21 17:53:09 +0400

    null

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pi::Application.config.secret_token = 'a15392eef8c6581b9b23b89960041dbf0f453c893e5d5e29c7d07b892503527c6ef853ceaf1ffc98aa74ff109676ac6501de35202bdb78cb5b2264d633f08055'
